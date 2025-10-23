24f7d8cffc06190553086a89d4ae4f346ee59b9b
Brandon Chuah <brandonchuah.sl@gmail.com>
2025-10-23 22:22:51 +0800

    chore: change root dir of packages

--- packages/configs/src/index.ts
+++ packages/configs/src/index.ts
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const configs = {
   fastForexApiKey: process.env.FAST_FOREX_API_KEY || "",
   brokerSignerPrivateKey: process.env.BROKER_SIGNER_PRIVATE_KEY || "",
 };
+
+console.log("configs in configs", configs);
