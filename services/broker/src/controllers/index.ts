8af2ff3ba2451513be10e4bbb1c53ca98683126c
Brandon Chuah <brandonchuah.sl@gmail.com>
2025-10-23 22:24:27 +0800

    chore: sign quote in controller

--- services/broker/src/controllers/index.ts
+++ services/broker/src/controllers/index.ts
@@ -1,4 +1,7 @@
+import { BrokerResponse } from "@fxswap/interfaces";
 import { NextFunction, Request, Response } from "express";
+import { FxRateService } from "../services/FxRate.service";
+import { SignatureService } from "../services/Signature.service";
 
 export async function quote(
   req: Request,
@@ -6,18 +9,36 @@ export async function quote(
   next: NextFunction,
 ): Promise<void> {
   try {
-    const { fromToken, toToken, receiver, amount } = req.body;
+    const { chainId, fromToken, toToken, receiver, amount } = req.body;
 
-    res.json({ ok: true, fromToken, toToken, receiver, amount });
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(
-        `quote error: ${err.message} - ${JSON.stringify(err.stack)}`,
-      );
-      next(err);
-    } else {
-      console.error(`quote error: ${JSON.stringify(err)}`);
-      next(new Error("unknown error"));
-    }
+    const fxRate = await FxRateService.getInstance().getRateFromTokenAddresses(
+      fromToken,
+      toToken,
+      chainId,
+      chainId,
+    );
+
+    const deadline = (Math.floor(Date.now() / 1000) + 120).toString(); // 2 minutes
+
+    const signature = await SignatureService.getInstance().signQuote(
+      { chainId, fromToken, toToken, receiver, amount },
+      fxRate,
+      deadline,
+    );
+
+    const response: BrokerResponse = {
+      chainId,
+      fromToken,
+      toToken,
+      receiver,
+      amount,
+      fxRate,
+      deadline,
+      signature,
+    };
+
+    res.json(response);
+  } catch (error) {
+    next(error);
   }
 }
