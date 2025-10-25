bf90c71a0d2c648133b1cbc7edd24b26a7391ee5
Brandon Chuah <brandonchuah.sl@gmail.com>
2025-10-25 17:56:42 +0800

    chore: update broker address

--- services/broker/src/constants/index.ts
+++ services/broker/src/constants/index.ts
@@ -2,4 +2,4 @@ import { Address } from "viem";
 
 // Safe address of broker
 export const BROKER_ADDRESS: Address =
-  "0x0000000000000000000000000000000000000000"; // TODO: set real address
+  "0xF16bD86AC718886F0550689A574D4552dEC0253E";
