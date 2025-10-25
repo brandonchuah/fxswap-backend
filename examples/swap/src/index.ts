166f41b15e1002c0fb12faf2942a9c9d28aafebe
Brandon Chuah <brandonchuah.sl@gmail.com>
2025-10-25 18:00:36 +0800

    chore: use swap in example

--- examples/swap/src/index.ts
+++ examples/swap/src/index.ts
@@ -1,22 +1,36 @@
 import { FxSwapClient } from "@fxswap/sdk";
-import { parseUnits } from "viem";
-import { generatePrivateKey, privateKeyToAddress } from "viem/accounts";
+import dotenv from "dotenv";
+import { createWalletClient, http, parseUnits } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
-async function main(): Promise<void> {
-  const client = new FxSwapClient(
-    process.env.ROUTER_URL || "http://localhost:3001",
-  );
+dotenv.config();
 
-  const res = await client.quote({
-    chainId: sepolia.id,
-    fromToken: "0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9", // PYUSD
-    toToken: "0x8a0c939571ef36363a5b4526a28ac59f623ebf97", // MockXSGD
-    receiver: privateKeyToAddress(generatePrivateKey()),
-    amount: parseUnits("1", 18).toString(),
+const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
+if (!PRIVATE_KEY) {
+  throw new Error("PRIVATE_KEY is not set");
+}
+
+async function main(): Promise<void> {
+  const account = privateKeyToAccount(PRIVATE_KEY);
+  console.log(account.address);
+
+  const walletClient = createWalletClient({
+    chain: sepolia,
+    transport: http(),
+    account,
   });
 
-  console.log(res);
+  const client = new FxSwapClient("http://localhost:3001");
+
+  const hash = await client.swap(walletClient, {
+    fromToken: "0xCaC524BcA292aaade2DF8A05cC58F0a65B1B3bB9", // PYUSD
+    toToken: "0x8a0c939571ef36363a5b4526a28ac59f623ebf97", // MockXSGD
+    receiver: account.address,
+    amount: parseUnits("1", 6).toString(),
+  });
+
+  console.log(`Transaction hash: ${hash}`);
 }
 
 main().catch((e) => {
